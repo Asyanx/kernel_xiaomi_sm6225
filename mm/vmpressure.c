eface346d7314b3ced85357c81a7894e27643c46
ardia-kun <ardiayogabekti@gmail.com>
2024-12-07 10:51:22 +0700

    fog: MGLRU fixups!
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -403,16 +403,7 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	reclaimed = vmpr->reclaimed;
 	spin_unlock(&vmpr->sr_lock);
 
-	if (scanned < vmpressure_win)
-		return;
-
-	spin_lock(&vmpr->sr_lock);
-	vmpr->scanned = 0;
-	vmpr->reclaimed = 0;
-	vmpr->stall = 0;
-	spin_unlock(&vmpr->sr_lock);
-
-	pressure = vmpressure_calc_pressure(scanned, reclaimed);
+ 	pressure = vmpressure_calc_pressure(scanned, reclaimed);
 	pressure = vmpressure_account_stall(pressure, stall, scanned);
 	vmpressure_notify(pressure);
 }
